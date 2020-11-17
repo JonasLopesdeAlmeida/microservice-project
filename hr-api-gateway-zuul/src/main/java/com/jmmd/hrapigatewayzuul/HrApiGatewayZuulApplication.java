1a555076a199a932abe3e778e7c83d77becd38c8
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-11-17 19:55:23 -0300

    Configuring zuul api gateway

--- hr-api-gateway-zuul/src/main/java/com/jmmd/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/jmmd/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,8 @@ package com.jmmd.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+@EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
