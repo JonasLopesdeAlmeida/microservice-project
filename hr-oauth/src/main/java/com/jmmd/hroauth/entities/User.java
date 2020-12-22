f743f5fe1fad9c928a1c5a9d28ac73da0ab09c31
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-12-22 18:24:12 +0000

    JWT token

--- hr-oauth/src/main/java/com/jmmd/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/jmmd/hroauth/entities/User.java
@@ -1,11 +1,17 @@
 package com.jmmd.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-public class User implements Serializable{
+public class User implements UserDetails, Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -91,6 +97,45 @@ public class User implements Serializable{
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return roles.stream()
+				//map catching roles and converting in another array of SimpleGrantedAuthority roles  
+				.map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
--- hr-oauth/src/main/java/com/jmmd/hroauth/servicies/UserService.java
+++ hr-oauth/src/main/java/com/jmmd/hroauth/servicies/UserService.java
@@ -3,19 +3,23 @@ package com.jmmd.hroauth.servicies;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.jmmd.hroauth.entities.User;
 import com.jmmd.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
 
+	//****this implementation has been used for tests****.
 	public User findByEmail(String email) {
 		//communicating with hr-user Microservices
 		//getBody catching user response entity 
@@ -25,6 +29,18 @@ public class UserService {
 	    	throw new IllegalArgumentException("Email not found");
 	    }
 	    logger.info("Email found : " + email);
+	    return user;
+	}
+
+	//this is the real method and implementation for an application.
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+	    if(user == null) {
+	    	logger.error("Email not found: " + username);
+	    	throw new UsernameNotFoundException("Email not found");
+	    }
+	    logger.info("Email found : " + username);
 	    return user;
 	}
 }
