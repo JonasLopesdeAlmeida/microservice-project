3fb556bf291b015316942986eaf6067d78c5f9a3
Jonas Lopes <jmdlopes.almeida@gmail.com>
2020-12-28 16:43:34 +0000

    db config server

--- hr-workers/src/main/java/com/jmmd/workers/resouces/WorkerResource.java
+++ hr-workers/src/main/java/com/jmmd/workers/resouces/WorkerResource.java
@@ -54,12 +54,13 @@ public class WorkerResource {
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id ){
  
-
+        /*
 		try {
 			Thread.sleep(3000L);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		*/
 		
 		//print the number port that is running
 		logger.info("PORT = " + env.getProperty("local.server.port") );
